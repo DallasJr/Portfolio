d060d04eb467944247bcd90581bd332c3d0c9872
DallasJr <49690481+DallasJr@users.noreply.github.com>
2024-09-25 11:48:39 +0200

    added gin gonic

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Route de test
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Bienvenue dans le backend en Go !"})
+	})
+
+	// Démarre le serveur sur le port 8080
+	router.Run(":8080")
+}
