4ae001a31b50719b7d80f0c4de1b1b61c3e4e9be
ijayanra <jayanraj.ilangovane@ynov.com>
2024-09-25 15:29:11 +0200

    mise a jours du main.go

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"portfolio/routes"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
@@ -13,5 +20,34 @@ func main() {
 	// Démarrer le serveur sur le port 8080
 	log.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
+
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(ctx)
+
+	moviesDatabase := client.Database("movies")
+	namesCollection := moviesDatabase.Collection("names")
+
+	// Utiliser bson.D pour créer un document BSON
+	namesOfMovies, err := namesCollection.InsertOne(ctx, bson.D{
+		{Key: "name", Value: "jkghfv"},
+		{Key: "title", Value: "lzkesghvjn"},
+	})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println(namesOfMovies.InsertedID)
 
 }
