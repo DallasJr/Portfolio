df69ab6cc14ca33f44536301773d80458ab82bd2
Ti bolom <noah.severin@ynov.com>
2024-09-25 16:56:25 +0200

    page d acceuil

---
+++ portfolio/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="App">
+      <header class="App-header">
+        <form>
+          <div class="menu-container">
+            <ul class="menu">
+              <li>Filmographie Acteur</li>
+              <li>Filmographie Réalisateur</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </form>
+      </header>
+      <div class="body">
+        <img src="/omarTourne.png" class="App-logo" alt="omar si" />
+        <h1>OMAR SI</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
